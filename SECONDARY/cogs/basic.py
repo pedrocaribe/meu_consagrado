59b6cde6b84cd48d8bd8b1527643c9df5ed6f820
Pedro Caribe <pho.caribe@gmail.com>
2023-08-30 23:09:50 -0300

    added docstrings to basic.py

--- SECONDARY/cogs/basic.py
+++ SECONDARY/cogs/basic.py
@@ -7,17 +7,34 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
+
 class Basic(commands.Cog):
+    """A Cog for basic commands.
+
+    This Cog provides basic commands for the bot.
+
+    Attributes:
+        bot: discord.ext.commands.Bot
+            The bot instance associated with the cog.
+    """
+
     def __init__(self, bot):
         self.bot = bot
-
-        '''Commands defined in this cog are for basic commands.
-
-                basic v1.00'''
 
     # Ping Pong - Ping command
     @app_commands.command(name='ping', description='Ping, Pong. Verificar ping do BOT')
     async def ping(self, interaction: discord.Interaction):
+        """A command to check the bot's ping.
+
+        This command calculates and displays the bot's ping in milliseconds.
+
+        Args:
+            interaction: discord.Interaction
+                The interaction object representing the command invocation.
+
+        Returns:
+            This function does Not return anything.
+        """
 
         lat = round(self.bot.latency * 1000)
         return await interaction.response.send_message(f'Pong!\n\n`Bot ping {lat} ms`')
@@ -25,6 +42,17 @@ class Basic(commands.Cog):
     # Ping Pong - Pong command
     @app_commands.command(name='pong', description='Pong, Ping')
     async def pong(self, interaction: discord.Interaction):
+        """A command to respond with "Ping!".
+
+        This command is a simple response to the user's pong command, saying "Ping!".
+
+        Args:
+            interaction: discord.Interaction
+                The interaction object representing the command invocation.
+
+        Returns:
+            This function does Not return anything.
+        """
 
         return await interaction.response.send_message('Ping!')
 
@@ -32,16 +60,28 @@ class Basic(commands.Cog):
     @app_commands.command(name='invite', description='Cria convite temporário por tempo pré-determinado')
     @commands.guild_only()
     async def invite(self, interaction: discord.Interaction):
+        """A command to create a temporary invite link for a predetermined duration.
+
+        This command generates a temporary invite link for the current channel with a maximum age of 5 minutes.
+
+        Args:
+            interaction: discord.Interaction
+                The interaction object representing the command invocation.
+
+        Returns:
+            This function does Not return anything.
+        """
 
         link = await interaction.channel.create_invite(max_age=300)
         await interaction.response.send_message(f'Convite válido por 5 minutos: {link}')
 
-    @app_commands.command(name='matematica', description='Resolva expressões matemáticas de forma rápida. Uso: !matematica ExpressaoMatematica')
+    @app_commands.command(name="matematica", description="Resolva expressões matemáticas de forma rápida")
     async def math(self, interaction: discord.Interaction, expression: str):
 
         symbols = ['+', '-', '/', '*', '%']
 
         if any(s in expression for s in symbols):
+
             calculated = eval(expression)
             
             embed = discord.Embed(
