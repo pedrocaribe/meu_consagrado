ca0ee8050180399a0e67fbc4bdccdf9a1704eece
Pedro Caribe <pho.caribe@gmail.com>
2023-08-31 18:24:09 -0300

    reorganized code and added docstrings for dice in gamble.py

--- SECONDARY/cogs/gamble.py
+++ SECONDARY/cogs/gamble.py
@@ -11,24 +11,44 @@ class Gamble(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        '''Commands defined in this cog are for entertainment of gambling commands.
-
-                        gamble v1.00'''
-
-    # Flip coin
     @app_commands.command(name='moeda', description='Jogue a moeda rapidamente.')
     async def coin(self, interaction: discord.Interaction):
+        """A command to quickly flip a coin.
 
-        # Random floating number between 0 and 1
-        a = random.random()
+        This command simulates a coin flip and sends the result as "Cara!" (Heads) or "Coroa!" (Tails).
+
+        Args:
+            interaction: discord.Interaction
+                The interaction object representing the command invocation.
+
+        Returns:
+            This function does Not return anything.
+        """
+
+        a = random.random()  # Random floating number between 0 and 1
 
         return await interaction.response.send_message('**Coroa!**' if a > 0.50 else '**Cara!**')
 
     # Roll dices
-    @app_commands.command(name='dados', description='Role os dados rapidamente. Uso (num de 1 a 3 representando a quantidadee de dados)')
+    @app_commands.command(
+        name='dados', description='Role os dados rapidamente. Uso (num de 1 a 3 representando a quantidadee de dados)')
     async def dice(self, interaction: discord.Interaction, dicenum: int = 1):
+        """A command to quickly roll dice.
 
-        await interaction.response.send_message(f'Rolando {dicenum} dados... 🎲🎲\n' if dicenum > 1 else f'Rolando dado... 🎲\n')
+        This command simulates rolling dice(s) and sends the results.
+
+        Args:
+            interaction: discord.Interaction
+                The interaction object representing the command invocation.
+            dicenum: int [OPTIONAL]
+                The number of dice to be rolled. Defaults to 1.
+
+        Returns:
+            This function does Not return anything.
+        """
+
+        await interaction.response.send_message(
+            f'Rolando {dicenum} dados... 🎲🎲\n' if dicenum > 1 else f'Rolando dado... 🎲\n')
         
         embed = discord.Embed(
             title='Dados', 
@@ -37,16 +57,15 @@ class Gamble(commands.Cog):
         )
 
         # Roll for each dice
-        for i in range(1, dicenum + 1):
-            
-            r = random.randint(1, 6)
-            
-            embed.add_field(
-                name=f'**__DADO {i}__**', 
-                value=f'**-> {r} <-**'
-            )
+        for i in range(dicenum):
 
-            await interaction.edit_original_response(embed=embed)
+            r = random.randint(1, 6)
+
+            embed.add_field(
+                name=f'**__DADO {i + 1}__**',
+                value=f'**-> {r} <-**')
+
+        await interaction.edit_original_response(embed=embed)
 
     # Greater than or smaller than 6 faced dices
     @app_commands.command(name='dd', description='Role dados de mais ou menos de 6 faces')
