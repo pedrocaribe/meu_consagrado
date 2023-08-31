fe54de6eac8971d2c359be92e6411cd8ec655c69
Pedro Caribe <pho.caribe@gmail.com>
2023-08-31 16:46:11 -0300

    added docstrings to cccheck in cs50.py

--- SECONDARY/cogs/cs50.py
+++ SECONDARY/cogs/cs50.py
@@ -17,29 +17,56 @@ class Cs50(commands.Cog):
 
     @app_commands.command(name="mario-less", description='Projeto criado durante Week 1/pset1 do curso CS50')
     async def mario_less(self, interaction: discord.Interaction, bricks: int):
+        """A command to print a Mario pyramid with specified number of bricks.
+
+        This command creates a pyramid of bricks similar to the ones seen in the game Mario.
+        The number of bricks in each row is determined by the user input.
+
+        Args:
+            interaction: discord.Interaction
+                The interaction object representing the command invocation.
+            bricks: int
+                The number of bricks to be used in the pyramid.
+
+        Returns:
+            This function does Not return anything.
+        """
 
         # Check if user inputted value between 1 and 8
         if not 1 <= bricks <= 8:
             return await interaction.response.send_message(
                 f'Quantidade deve ser entre 1 e 8, **{random.choice(FRASE_MEIO)}**.')
 
-        # Send bricks increasing in one message
+        # Send bricks increasing all in one message
         text = ''
         for i in range(bricks):
             j = (bricks - i) - 1
             text += ('...' * j + '#' * (i + 1) + '\n')
         await interaction.response.send_message(text)
 
-    # Mario More Function
     @app_commands.command(name='mario-more', description='Projeto criado durante Week 1/pset1 do curso CS50. Uso: QtDeAndares (de 1 a 8)')
     async def mario_more(self, interaction: discord.Interaction, bricks: int):
+        """A command to print a Mario pyramid with mirroed specified number of bricks.
+
+        This command creates a pyramid of bricks similar to the ones seen in the game Mario.
+        The number of bricks in each row side is determined by the user input.
+
+        Args:
+            interaction: discord.Interaction
+                The interaction object representing the command invocation.
+            bricks: int
+                The number of bricks to be used in the pyramid.
+
+        Returns:
+            This function does Not return anything.
+        """
 
         # Check if user inputted value between 1 and 8
         if not 1 <= bricks <= 8:
             return await interaction.response.send_message(
                 f'Quantidade deve ser entre 1 e 8, **{random.choice(FRASE_MEIO)}**.')
 
-        # Send bricks increasing in one message
+        # Send bricks increasing all in one message
         text = ''
         for i in range(bricks):
             j = (bricks - i) - 1
@@ -50,6 +77,21 @@ class Cs50(commands.Cog):
     # Credit Card Check Function
     @app_commands.command(name='cccheck', description='Projeto criado durante Week 1/pset1 do curso CS50. Uso: NumeroDoCartao')
     async def cccheck(self, interaction: discord.Interaction, ccnumber: int):
+        """
+        A command to check the validity of a credit card number using the Luhn's Algorithm.
+
+        This command checks whether a given credit card number is valid or not based on the Luhn's Algorithm.
+        It also determines the vendor (VISA, MasterCard, AMEX, or INVALID) based on the number's length and range.
+
+        Args:
+            interaction: discord.Interaction
+                The interaction object representing the command invocation.
+            ccnumber: int
+                The credit card number to be checked.
+
+        Returns:
+            This function does Not return anything.
+        """
 
         # Initialize variables
         ccnumber2 = ccnumber // 10
