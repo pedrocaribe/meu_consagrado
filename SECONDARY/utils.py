84a559e16c2a909b3526a51f834c675eca380672
Pedro Caribe <pho.caribe@gmail.com>
2023-09-09 17:33:28 -0300

    refactored db_connect function to create tables at first use

--- SECONDARY/utils.py
+++ SECONDARY/utils.py
@@ -39,6 +39,34 @@ async def get_jokes():
 def db_connect(db):
     if os.path.isfile(db):
         dbc = sqlite3.connect(db)
+        cur = dbc.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS guilds ( "
+                    "guild_id INTEGER PRIMARY KEY, "
+                    "guild_name TEXT, "
+                    "guild_owner_id INTEGER, "
+                    "joined_date DATE, "
+                    "premium BOOL DEFAULT False NOT NULL,  "
+                    "active BOOL DEFAULT False NOT NULL); "
+
+                    "CREATE TABLE IF NOT EXISTS messages ( "
+                    "guild_id INTEGER, "
+                    "channel_id INTEGER, "
+                    "message_id INTEGER, "
+                    "author_id INTEGER, "
+                    "date DATE, "
+                    "time TIME, "
+                    "content TEXT, "
+                    "PRIMARY KEY (channel_id, message_id)) WITHOUT ROWID; "
+
+                    "CREATE TABLE IF NOT EXISTS tickets ( "
+                    "ticket_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    "guild_id INTEGER, "
+                    "channel_id INTEGER, "
+                    "message_id INTEGER, "
+                    "timestamp TEXT, "
+                    "error TEXT, "
+                    "user_id INTEGER, "
+                    "status TEXT CHECK (status IN ('OPEN', 'CLOSED', 'INPROGRESS')) NOT NULL DEFAULT 'OPEN');")
         return dbc
     else:
         return None
