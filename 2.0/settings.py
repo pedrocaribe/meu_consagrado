123f97517af3577dd70cb9778ce38647f90dd4ab
Pedro Caribe <pho.caribe@gmail.com>
2023-09-10 14:29:59 -0300

    improved print statement to reset colors after first line

--- 2.0/settings.py
+++ 2.0/settings.py
@@ -10,11 +10,11 @@ if DEBUG:
     env_path = Path(".") / ".env.debug"
     load_dotenv(dotenv_path=env_path)
     from settings_files.development import *
-    print(f"{Fore.WHITE + Back.YELLOW}We are in DEBUG mode{Back.RESET}")
+    print(f"{Fore.WHITE + Back.YELLOW}We are in DEBUG mode{Style.RESET_ALL}")
 
 else:
 
     env_path = Path(".") / ".env"
     load_dotenv(dotenv_path=env_path)
     from settings_files.production import *
-    print(f"{Fore.WHITE + Back.RED}CAREFUL We are in PRODUCTION mode{Back.RESET}")
+    print(f"{Fore.WHITE + Back.RED}CAREFUL We are in PRODUCTION mode{Style.RESET_ALL}")
